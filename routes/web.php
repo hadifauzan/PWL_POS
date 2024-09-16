3caba596f95a00197ba19aafd76e281cae763be9
hadi fauzan <mfauzan355@gmail.com>
2024-09-16 17:24:04 +0700

    js 3 praktikum 5 & 6 query builder dan eloquent orm

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +21,5 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController :: class, 'index']) ;
+Route::get('/user', [UserController::class, 'index']);
